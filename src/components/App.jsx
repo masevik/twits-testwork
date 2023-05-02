19aa9041dfc86a1f2c8b274059afce3f58e470b6
Serhii Malychkovych <masevik@gmail.com>
2023-05-02 16:41:57 +0300

    fix route 404 with Navigate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import Home from 'pages/Home';
 import Tweets from 'pages/Tweets';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/tweets" element={<Tweets />}></Route>
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
